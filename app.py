6ff6560595f6b21418934b6ceacce6469f52c88b
jeremysteele <jeremysteelewriter@gmail.com>
2021-02-04 15:44:03 -0800

    main

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction.text import CountVectorizer 
+import random
 
 # Loading the bag of soup dataframe
 main_df = pd.read_csv("big_movies_csv/the_main_df.csv")
@@ -32,8 +33,17 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-	
-	return render_template("index.html")
+	all_movies = main_df.to_json(orient='index') 
+	return render_template("index.html", all_movies=all_movies)
+
+@app.route("/random")
+def random():
+    x = random.randint(0, len(main_df))
+    single_movie = main_df['title'][x]
+
+    recommend_list = recommend(single_movie)
+
+    return render_template("title.html", recommend_list=recommend_list, title=single_movie)
 
 @app.route("/title/<title>")
 def title(title):
