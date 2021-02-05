e6d8995b08413bcfc867e89ad5b9ac7f71f7262d
jeremysteele <jeremysteelewriter@gmail.com>
2021-02-04 17:43:31 -0800

    more

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -3,6 +3,7 @@ document.write('\
         <div class="top-bar topbar-center-logo" id="topbar-center-logo">\
             <div class="title-bar-center">\
         		<a href="/">Home</a>\
+                <img src="https://www.pngitem.com/pimgs/m/30-307272_transparent-movie-camera-clipart-movie-camera-logo-png.png"width="200" height="100" />\
         		<a href="/random">Random Movie</a>\
             </div>\
         </div>\
